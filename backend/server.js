1efcf1b13fa7ceea4ea3c647ebd5392cd440787a
luis casanovas <luisvallsc@gmail.com>
2024-06-19 18:45:49 +0200

    Update .gitignore file to exclude all node_modules

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const port = ({}).PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/cities', require('./routes/cities'))
+
 
 app.listen(port, () => {
   console.log("Server is running on " + port);
